59a0418ccd2c9498cc7f2a3f80b8ec47b8e11e99
iam454 <realseogy@gmail.com>
2023-05-18 11:23:59 +0900

    feat: Compare 섹션 렌더링 함수 생성

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import ipads from "../data/ipads.js";
+
 // 장바구니
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
@@ -95,4 +97,32 @@ pauseBtn.addEventListener("click", () => {
   video.pause();
   pauseBtn.classList.add("hide");
   playBtn.classList.remove("hide");
+});
+
+// 당신에게 맞는 iPads는? 렌더링
+const itemsEl = document.querySelector("section.compare .items");
+
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+  let colorList = "";
+  ipad.colors.forEach(function (color) {
+    colorList += /* html */ `
+      <li style="background-color: ${color}"></li>
+    `;
+  });
+  itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}"/>
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString("en-US")}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+  itemsEl.append(itemEl);
 });
